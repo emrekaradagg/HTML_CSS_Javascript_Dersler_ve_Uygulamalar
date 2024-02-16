28d875042a258b3b8ce07a15fc85452c414e7b0e
emre <emre>
2024-02-16 16:25:47 +0300

    init

---
+++ JAVASCRİPT/Enes Bayram/65 TodoList Projesi Bilgilendirme Mesajı Yapma/app.js
@@ -0,0 +1,90 @@
+//Tüm Elementleri Seçmek
+
+const form = document.querySelector("#todoAddForm");
+const addInput = document.querySelector("#todoName");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+const clearButton = document.querySelector("#clearButton");
+
+let todos=[];
+
+runEvents();
+
+function runEvents(){
+    form.addEventListener("submit",addTodo);
+}
+
+function addTodo(e){
+    const inputText = addInput.value.trim();
+    if(inputText==null || inputText==""){
+        showAlert("warning","Lütfen boş bırakmayınız!");
+    }else{
+     //Arayüz ekleme
+        addTodoToUI(inputText);
+        addTodoToStorage(inputText);
+        showAlert("success","Todo Eklendi");
+    }
+     //Storage ekleme
+    e.preventDefault();
+}
+
+function addTodoToUI(newTodo){
+   /*
+   <li class="list-group-item d-flex justify-content-between">Todo 1
+                                    <a href="#" class="delete-item">
+                                        <i class="fa fa-remove"></i>
+                                    </a>
+                                </li>
+                                */
+   const li = document.createElement("li");                           
+    li.className = "list-group-item d-flex justify-content-between";
+    li.textContent = newTodo;
+
+    const a = document.createElement("a");
+    a.href="#";
+    a.className = "delete-item";
+
+    const i = document.createElement("i");
+    i.className = "fa fa-remove";
+
+    a.appendChild(i);
+    li.appendChild(a);
+    todoList.appendChild(li);
+
+    addInput.value ="";
+}
+
+function addTodoToStorage(newTodo){
+   checkTodosFromStorage();
+   todos.push(newTodo);
+   localStorage.setItem("todos",JSON.stringify(todos));
+}
+
+function checkTodosFromStorage(){
+     if(localStorage.getItem("todos")===null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem("todos"))
+    }
+}
+
+function showAlert(type, message){
+//     <div class="alert alert-warning" role="alert">
+//          This is a warning alert—check it out!
+//    </div>
+
+const div = document.createElement("div");
+// div.className ="alert alert-"+type;
+div.className = 'alert alert-${type}';
+div.textContent = message;
+
+firstCardBody.appendChild(div);
+
+setTimeout(function(){
+div.remove();
+},2000)
+
+
+}
+
