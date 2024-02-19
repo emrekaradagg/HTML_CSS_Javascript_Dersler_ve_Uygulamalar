2569afc69e644507ed3314b6eeeb58a24d3a8934
emre <emre>
2024-02-19 16:44:56 +0300

    init

---
+++ JAVASCRİPT/Enes Bayram/71 Arrow Function Kullanımı  ES6/app.js
@@ -0,0 +1,38 @@
+// function yazdir(){
+//     console.log("Merhaba");
+// }
+
+// yazdir();
+
+// const yazdir = ()=>{
+//     console.log("Merhaba");
+// }
+// yazdir();
+
+// const yazdir =(firstName,lastName)=>{
+//     console.log("Merhaba",firstName,lastName)
+// }
+
+// yazdir("Emre","Karadağ")
+
+// ! Eğer süslü parantez {} kullanmazsak içerideki değerleri rastgele ekrana yazdırır 
+
+// const yazdir =(firstName,lastName)=>
+//     console.log("Merhaba",firstName,lastName)
+//     let a = 6;
+//     console.log(a);
+//     console.log("JS dersleri")
+
+
+// yazdir("Emre","Karadağ")
+
+// ! Tek bir parametre kullanıyorsa oval parantez () kullanılmasa da olur
+
+// const yazdir = firstName => console.log("Merhaba",firstName)
+
+// yazdir("Emre")
+
+const kupAl = x=> x*x*x
+
+
+console.log("Değer",kupAl(3));
