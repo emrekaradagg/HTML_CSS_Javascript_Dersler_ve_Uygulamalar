029cdb07dd5e357ab06d8ad9492c512bf9a3837a
emre <emre>
2024-02-22 15:20:25 +0300

    init

---
+++ JAVASCRİPT/Enes Bayram/77 Set Kullanımı ES6/app.js
@@ -0,0 +1,54 @@
+//SET
+
+const set = new Set();
+
+//add metodu
+
+set.add(true)
+set.add(3.14)
+set.add("Emre")
+// set.add("Emre")
+// set.add("Emre")
+// set.add("Emre")
+// set.add("Emre")
+// set.add("Emre")
+set.add(7)
+set.add({username :"emre", password : "1"});
+set.add(1,2,3,4)
+
+//Size
+// console.log(set.size)
+
+//delete
+
+// set.delete("Emre")
+// set.delete(7)
+// set.delete(1,2,3,4)
+// console.log(set.size)
+
+//Has
+
+// console.log(set.has("Emre"));
+
+//For of döngüsü
+
+// for(let value of set){
+//     console.log(value)
+// }
+
+//SET den Array Oluşturma
+
+// const values = Array.from(set);
+// values.forEach((value)=>{
+// console.log(value)
+// })
+
+//ARRAY den SET Oluşturma
+
+// let array = [1,"Enes",true,"Mustafa",15,[1,2,3]]
+
+// const newSet = new Set(array);
+// console.log(newSet)
+
+
+
---
+++ JAVASCRİPT/Enes Bayram/78 Template Literals Kullanımı/app.js
@@ -0,0 +1,13 @@
+//TEMPLAT LİTERALS
+
+// function write(firsName , lastName){
+//     console.log(`İsim : ${firsName} Soyisim : ${lastName}`)
+// }
+
+// write("Emre", "Karadağ")
+
+// function getUserById(userId){
+//     console.log(`http://localhost:8080/user/${userId}`)
+// }
+
+// getUserById(10)
