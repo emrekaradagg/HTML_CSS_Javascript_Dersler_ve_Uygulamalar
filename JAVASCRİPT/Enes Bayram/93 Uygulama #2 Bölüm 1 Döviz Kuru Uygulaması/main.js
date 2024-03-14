08740fa25cd4726b8a4d158c3aff12b361c41c41
emre <emre>
2024-03-14 14:09:50 +0300

    init

--- JAVASCRİPT/Enes Bayram/93 Uygulama #2 Bölüm 1 Döviz Kuru Uygulaması/main.js
+++ JAVASCRİPT/Enes Bayram/93 Uygulama #2 Bölüm 1 Döviz Kuru Uygulaması/main.js
@@ -5,6 +5,8 @@ const firstOption = document.querySelector("#firstCurrencyOption");
 const secondOption = document.querySelector("#secondCurrencyOption");
 const resultInput = document.querySelector("#result");
 
+const currency = new Currency();
+
 runEventListeners();
 
 function runEventListeners(){
@@ -13,8 +15,12 @@ function runEventListeners(){
 
 function exchange(){
     const amount = Number(amountInput.value.trim());
-    const firstOptionValue = firstOption.options[firstOption.selectedIndex].texContent;
-    const secondOptionValue = secondOption.options[secondOption.selectedIndex].texContent;
+    const firstOptionValue = firstOption.options[firstOption.selectedIndex].textContent;
+    const secondOptionValue = secondOption.options[secondOption.selectedIndex].textContent;
     
-    console.log(firstOptionValue , secondOptionValue)
+    currency.exchange(amount,firstOptionValue,secondOptionValue)
+    .then((result)=> {
+        resultInput.value = result.toFixed(2)
+    })
+    
 }
